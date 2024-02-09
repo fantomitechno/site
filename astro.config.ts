05119528d6473e756a23e5222937812ef02f874d
Simon - fantomitechno <simon@renoux.dev>
2024-02-09 13:58:22 +0100

    🎉 Create an other version

---
+++ astro.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+import node from "@astrojs/node";
+import image from './build/image.js';
+
+export default defineConfig({
+  site: "https://simon.renoux.dev",
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [image()]
+});
